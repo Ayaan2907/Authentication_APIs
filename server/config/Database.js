66393e9e2e0a8d8daed9b6fd7c94d97b8af411e2
Ayaan <kaifullah7921@gmail.com>
2022-09-13 03:44:55 +0530

    database.js file is responsible for establishment of connection with mysql. It will be used in api as service

--- server/config/Database.js
+++ server/config/Database.js
@@ -10,7 +10,21 @@ const pool = mysql.createPool({
     // database: process.env.DATABASE,
     connectionLimit: 10,
 });
-
+pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === "PROTOCOL_CONNECTION_LOST") {
+            console.error("Database connection was closed.");
+        }
+        if (err.code === "ER_CON_COUNT_ERROR") {
+            console.error("Database has too many connections.");
+        }
+        if (err.code === "ECONNREFUSED") {
+            console.error("Database connection was refused.");
+        }
+    }
+    if (connection) connection.release();
+    return;
+});
 console.log(process.env);
 
 module.exports = pool;

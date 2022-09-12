54f81c1ab77da215008934eed850d726c54da9f7
Ayaan <kaifullah7921@gmail.com>
2022-09-13 03:47:15 +0530

    it imports the database service and provides functionality for that passsing the data from the request body. these controllers are exported to routers

---
+++ server/api/controller.js
@@ -0,0 +1,59 @@
+const {
+    createUserService,
+    getAllUsersService,
+    deleteUserService,
+} = require("./service");
+// const { hashSync, genSaltSync } = require("bcrypt");
+
+module.exports = {
+    createUser: (req, res) => {
+        const body = req.body;
+        // const salt = genSaltSync(10);
+        // body.password = hashSync(body.password, salt);
+        console.log(body);
+        createUserService(body, (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    success: 0,
+                    message: err.message,
+                });
+            }
+            return res.status(200).json({
+                success: 1,
+                data: results,
+            });
+        });
+    },
+    getAllUsers: (req, res) => {
+        getAllUsersService((err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    success: 0,
+                    message: err.message,
+                });
+            }
+            return res.status(200).json({
+                success: 1,
+                data: results,
+            });
+        });
+    },
+    deleteUser: (req, res) => {
+        const id = req.params.id;
+        deleteUserService(id, (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    success: 0,
+                    message: err.message,
+                });
+            }
+            return res.status(200).json({
+                success: 1,
+                message: "user deleted successfully",
+            });
+        });
+    },
+};

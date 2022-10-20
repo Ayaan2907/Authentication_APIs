7c863c98883f0b5ed9d4aa90949ed1f29e7b5c62
Ayaan <kaifullah7921@gmail.com>
2022-10-20 12:05:23 +0530

    added dummy pi chart, later to be handeld by asm

--- client/src/components/DeviceData.js
+++ client/src/components/DeviceData.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { PieChart, Pie } from 'recharts';
+import FetchDataByIdSearvice from '../services/FetchDataService';
 
 export default function DeviceData() {
-  return (
-    <div>DeviceData</div>
-  )
-}
+    let { id } = useParams();
+    const [gpsData, setGpsData] = useState([]);
+    useEffect(() => {
+        setTimeout(() => {
+        FetchDataByIdSearvice(id).then((data) => {
+            setGpsData(data);
+        });
+        }, 4000);
+    }, [gpsData]);
+
+    // TODO: handle logic for calculating location and generate pie based on that
+    
+    const data = [
+        {name: 'Geeksforgeeks', students: 400},
+        {name: 'Technical scripter', students: 700},
+        {name: 'Geek-i-knack', students: 200},
+        {name: 'Geek-o-mania', students: 1000}
+      ];
+        
+        
+    return (<>
+        <PieChart width={400} height={400}>
+            <Pie data={data} dataKey="students" cx="50%" cy="50%" outerRadius={80} fill="#8884d8" label />
+        </PieChart>
+
+    </>);
+      }

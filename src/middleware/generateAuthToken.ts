e577d685e2d107fb045ef8ce7c470551e8f3e4b7
Ayaan2907 <ayaanansari7921@outlook.com>
2023-03-16 17:52:55 +0530

    function to generate jwt token

--- src/middleware/generateAuthToken.ts
+++ src/middleware/generateAuthToken.ts
@@ -1,65 +1,29 @@
-import { Request, Response, NextFunction } from "express";
+import { Response } from "express";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import mongoose from "mongoose";
-import { User } from "../types/user.type.js";
+import { IUser } from "../types/user.type.js";
 import Logging from "../library/logging.js";
-import UserInstance from "../models/user.model.js";
 import config from "../config/config.js";
+import commonErrorActions from "../types/error.type.js";
 
-const generateAuthToken = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
-    // const { email, password } = req.body;
-    // if (!email || !password) {
-    //     Logging.warning("Missing fields");
-    //     return res.status(400).json({ error: "Missing fields" });
-    // }
-    // try {
-    //     // if (!mongoose.isValidObjectId(email)) {
-    //     //     Logging.warning("Invalid email");
-    //     //     return res.status(400).json({ error: "Invalid email" });
-    //     // }
-
-    //     await UserInstance.findOne({ email }, (err: Error, user: User) => {
-    //         if (err) {
-    //             Logging.error(err);
-    //             return res.status(500).json({ error: err });
-    //         }
-    //         if (!user) {
-    //             Logging.warning("User not found");
-    //             return res.status(404).json({ error: "User not found" });
-    //         }
-    //         bcrypt.compare(password, user.password, (err, isPasswordValid) => {
-    //             if (err) {
-    //                 Logging.error(err);
-    //                 return res.status(500).json({ error: err });
-    //             }
-    //             if (!isPasswordValid) {
-    //                 Logging.warning("Invalid password");
-    //                 return res.status(400).json({ error: "Invalid password" });
-    //             }
-    //             Logging.event(`user.name: ${user.name} logged in`);
-
-    //             const token = jwt.sign(
-    //                 { _id: user.id, email: user.email },
-    //                 config.jwt.JWT_SECRET,
-    //                 {
-    //                     expiresIn: "1h",
-    //                 }
-    //             );
-    //             Logging.event(`Token generated for user: ${user.name}`);
-    //             res.header("x-auth-token", token).json({ token });
-
-    //             next();
-    //         });
-    //     });
-    // } catch (error) {
-    //     Logging.error(error);
-    //     res.status(500).json({ error });
-    // }
+const generateAuthToken = async (user: IUser, res: Response) => {
+    try {
+        const token = jwt.sign(
+            {
+                _id: user.id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            },
+            config.jwt.JWT_SECRET,
+            {
+                expiresIn: config.jwt.JWT_EXPIRY_TIME,
+            }
+        );
+        Logging.event(`Token generated for user: ${user.name}`);
+        return token;
+    } catch (error) {
+        commonErrorActions.other(res, error);
+    }
 };
 
 export default generateAuthToken;

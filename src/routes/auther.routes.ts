6abadc1b7ab6d2398dbe6a4d256c43dc9ee91068
Ayaan2907 <ayaanansari7921@outlook.com>
2023-03-17 02:38:29 +0530

    some more changes in routes

--- src/routes/auther.routes.ts
+++ src/routes/auther.routes.ts
@@ -5,12 +5,12 @@ const authRoutes = express.Router();
 
 // Public routes
 authRoutes.post("/signup", controller.createUser);
-authRoutes.get("/signin", controller.loginUser);
-authRoutes.get("/user/:id", controller.getUser);
+authRoutes.post("/signin", controller.loginUser);
 
 // Protected routes
-authRoutes.get("/all-users /", decodeAuthToken, controller.getAllUsers);
-authRoutes.patch("/user/:id", decodeAuthToken, controller.updateUser);
+authRoutes.get("/all-users", decodeAuthToken, controller.getAllUsers);
+authRoutes.get("/user/:id", decodeAuthToken, controller.getUser);
+authRoutes.put("/user/:id", decodeAuthToken, controller.updateUser);
 authRoutes.delete("/user/:id", decodeAuthToken, controller.deleteUser);
 
 export default authRoutes;
